1a286bf229b4464a0ab05ee5870ea45c1bdbda93
jacobobendrado <jacobobendrado@gmail.com>
2023-11-09 22:00:01 -0500

    Whitespace and shortening lines

--- src/main/java/com/mycompany/project4_algos/Project4_Algos.java
+++ src/main/java/com/mycompany/project4_algos/Project4_Algos.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 
-
 /**
  * MainClass for Project 4
  *
@@ -13,9 +12,9 @@ import java.util.Queue;
  * Created 11/9/2023
  * Summary of Modifications:
  *
- * Description: Main class where project4 is written and executed.
- * 
- *
+ * Description: Main class where project4 is written and executed. It uses a 
+ *  breadth-first search implemented with a queue and a hashmap to implement
+ *  memoization for dynamic programming.
  */
 public class Project4_Algos {
     
@@ -52,12 +51,11 @@ public class Project4_Algos {
             garage[i] = myVehicle;
         }
         
-        //create Queue
+        //create Queue with initial PositionString
         Queue<String> RHQueue = new LinkedList();
-        //add initial PositionString
         RHQueue.add(startPositionString);
         
-        //create hash map and add initial move string
+        //create hash map and add initial move string with sentinal parent
         HashMap<String, String> prevMoves = new HashMap<>();
         prevMoves.put(startPositionString, "winnerwinnerchickendinnah");
         
@@ -66,77 +64,95 @@ public class Project4_Algos {
         //work through the Queue that contains all the possible strings
         outer:
         while (!RHQueue.isEmpty()){
-            //remove current board to test
+            //get current string to test
             currentPositionString = RHQueue.remove();
             
             //check each car's move
             for (int i = 0; i < garage.length; i++) {
                 Vehicle currVehicle = garage[i];
-                
                 int dynamicCoord = currentPositionString.charAt(i) - '0';
-                //check orientati0n
-                if (currVehicle.getOrient()=='v'){
+                
+                //check orientation
+                if (currVehicle.getOrient() == 'v'){
                     //check up until can't
-                    for (int j = dynamicCoord-1; j > 0; j--){
+                    for (int j = dynamicCoord - 1; j > 0; j--){
                         if (!checkMove(garage, currentPositionString, j, currVehicle.getStaticPos())){
                             break;
                         }
-                        //create new board reflecting move
-                        String newString = currentPositionString.substring(0,i)+String.valueOf(j)+currentPositionString.substring(i+1);
                         
-                        //call finish function
+                        //create new board reflecting move
+                        String newString = currentPositionString.substring(0, i)
+                                         + String.valueOf(j)
+                                         + currentPositionString.substring(i + 1);
+                        
+                        //check if the red car, which is the first car, is at the end of the row
                         if (currentPositionString.charAt(0) == '5'){
                             break outer;
-                        } else if (!prevMoves.containsKey(newString)) {
+                        } 
+                        //add new strings to the queue and hashmap
+                        else if (!prevMoves.containsKey(newString)) {
                             RHQueue.add(newString);
                             prevMoves.put(newString, currentPositionString);
                         }
                     }
                     //check down until can't
-                    for (int j = dynamicCoord+currVehicle.getLength(); j < 7; j++){
+                    for (int j = dynamicCoord + currVehicle.getLength(); j < 7; j++){
                         if (!checkMove(garage, currentPositionString, j, currVehicle.getStaticPos())){
                             break;
                         }
-                        //create new board reflecting move
-                        String newString = currentPositionString.substring(0,i)+String.valueOf(j-currVehicle.getLength()+1)+currentPositionString.substring(i+1);
                         
-                        //call finish function
+                        //create new board reflecting move
+                        String newString = currentPositionString.substring(0, i)
+                                         + String.valueOf(j - currVehicle.getLength() + 1)
+                                         + currentPositionString.substring(i + 1);
+                        
+                        //check if the red car, which is the first car, is at the end of the row
                         if (currentPositionString.charAt(0) == '5'){
                             break outer;
-                        } else if (!prevMoves.containsKey(newString)) {
+                        } 
+                        //add new strings to the queue and hashmap
+                        else if (!prevMoves.containsKey(newString)) {
                             RHQueue.add(newString);
                             prevMoves.put(newString, currentPositionString);
                         }
                     }
                 } else {
                     //check left until can't
-                    for (int j = dynamicCoord-1; j > 0; j--){
+                    for (int j = dynamicCoord - 1; j > 0; j--){
                         if (!checkMove(garage, currentPositionString, currVehicle.getStaticPos(), j)){
                             break;
                         }
                         //create new board reflecting move
-                        String newString = currentPositionString.substring(0,i)+String.valueOf(j)+currentPositionString.substring(i+1);
+                        String newString = currentPositionString.substring(0, i)
+                                         + String.valueOf(j)
+                                         + currentPositionString.substring(i + 1);
                         
-                        //call finish function
+                        //check if the red car, which is the first car, is at the end of the row
                         if (currentPositionString.charAt(0) == '5'){
                             break outer;
-                        } else if (!prevMoves.containsKey(newString)) {
+                        } 
+                        //add new strings to the queue and hashmap
+                        else if (!prevMoves.containsKey(newString)) {
                             RHQueue.add(newString);
                             prevMoves.put(newString, currentPositionString);
                         }
                     }
                     //check right until can't
-                    for (int j = dynamicCoord+currVehicle.getLength(); j < 7; j++){
+                    for (int j = dynamicCoord + currVehicle.getLength(); j < 7; j++){
                         if (!checkMove(garage, currentPositionString, currVehicle.getStaticPos(), j)){
                             break;
                         }
                         //create new board reflecting move
-                        String newString = currentPositionString.substring(0,i)+String.valueOf(j-currVehicle.getLength()+1)+currentPositionString.substring(i+1);
+                        String newString = currentPositionString.substring(0,i)
+                                         + String.valueOf(j - currVehicle.getLength() + 1)
+                                         + currentPositionString.substring(i + 1);
                         
-                        //call finish function
+                        //check if the red car, which is the first car, is at the end of the row
                         if (currentPositionString.charAt(0) == '5'){
                             break outer;
-                        } else if (!prevMoves.containsKey(newString)) {
+                        } 
+                        //add new strings to the queue and hashmap
+                        else if (!prevMoves.containsKey(newString)) {
                             RHQueue.add(newString);
                             prevMoves.put(newString, currentPositionString);
                         }
@@ -148,30 +164,36 @@ public class Project4_Algos {
         printMove(currentPositionString, prevMoves, garage, 0);
     }
     
-    //check if potential move is valid or not
+    //check if potential move is valid or not based only on if other cars are occupying the spot
     private static boolean checkMove(Vehicle[] garage, String currentPositionString, int y, int x){
-        //itierate through the list of cars to determine if any overlap with possible move
+        //iterate through the list of cars to determine if any overlap with possible move
         for (int i = 0; i < garage.length; i++){
             Vehicle curr = garage[i];
+            
             //determine orientation to properly disect move
             if (curr.getOrient() == 'h'){
                 if (y == curr.getStaticPos()){
-                    int dymPos = currentPositionString.charAt(i)-'0';
+                    int dymPos = currentPositionString.charAt(i) - '0';
+                    
                     //check for overlap
-                    if (dymPos <= x && x < dymPos+curr.getLength()){
+                    if (dymPos <= x && x < dymPos + curr.getLength()){
                         return false;
                     }
                 }
-            } else {
+            } 
+            else {
                 if (x == curr.getStaticPos()){
-                    int dymPos = currentPositionString.charAt(i)-'0';
+                    int dymPos = currentPositionString.charAt(i) - '0';
+                    
                     //check for overlap
-                    if (dymPos <= y && y < dymPos+curr.getLength()){
+                    if (dymPos <= y && y < dymPos + curr.getLength()){
                         return false;
                     }
                 }
             }
         }
+        
+        //no collisions
         return true;
     }
     
@@ -179,20 +201,28 @@ public class Project4_Algos {
     public static void printMove(String curr, HashMap<String, String> prevMoves, Vehicle []garage, int count){
         //get previous move
         String parent = prevMoves.get(curr);
+        
         //base case for recursion
         if (parent.equals("winnerwinnerchickendinnah")){
-            System.out.println(count + " moves:");
+            if (count == 1) {
+                System.out.println(count + " move:");
+            }
+            else {
+                System.out.println(count + " moves:");
+            }
             return;
         }
-        //recursive call
+        
+        //recursive call before printing
         printMove(parent, prevMoves, garage, count + 1);
+        
         //iterate through both current and parent strings to determine what has changed
         for (int i = 0; i < garage.length; i++){
             //find character that has changed
             if (parent.charAt(i) != (curr.charAt(i))){
-                
                 //construct string for printing move
                 String outString = garage[i].getName() + " ";
+                
                 if (parent.charAt(i) > curr.charAt(i)) {
                     outString += String.valueOf(parent.charAt(i) - curr.charAt(i)) + " ";
                     if (garage[i].getOrient() == 'h') {
--- src/main/java/com/mycompany/project4_algos/Vehicle.java
+++ src/main/java/com/mycompany/project4_algos/Vehicle.java
@@ -8,13 +8,21 @@ package com.mycompany.project4_algos;
  * Created 11/9/2023
  * Summary of Modifications:
  *
- * Description: data object that holds static information for each vehicle on board.
- * 
+ * Description: Data object that holds static information for each vehicle on 
+ *  the board.
  *
  */
 public class Vehicle {
 
+    private String name;
+    private char orientation;
+    private int length;
+    private int staticPos;
+    
+    
+    //Constructor
     public Vehicle(String type, String color, String orient, int position){
+        //If it's a car, length is 2, a truck has length of 3
         if (type.charAt(0) == 'c'){
             length = 2;
         } else {
@@ -24,28 +32,24 @@ public class Vehicle {
         orientation = orient.charAt(0);
         staticPos = position;
     }
-    private String name;
-    private char orientation;
-    private int length;
     
-    private int staticPos;
-    
+    //Getter for orientation
     public char getOrient(){
         return orientation;
     }
     
+    //Getter for length
     public int getLength(){
         return length;
     }
     
+    //Getter for the static part of the coordinate (either row or column)
     public int getStaticPos(){
         return staticPos;
     }
     
-    
+    //Getter for the name
     public String getName(){
         return name;
     }
-    
-    
 }
